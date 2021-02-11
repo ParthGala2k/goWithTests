313e77191e1802dc4faeccc3a927894a5ec706c5
Parth Gala <parthpgala@gmail.com>
2021-02-11 09:56:32 +0000

    Refactor hello_test.go to avoid redundancy and focus on reusability by declaring a function and assigning the function to a variable.

--- src/hello/hello_test.go
+++ src/hello/hello_test.go
@@ -2,20 +2,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	t.Run("saying hello with arguments", func(t *testing.T){
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
+	assertCorrectMessage := func(t testing.TB, got, want string){
+		t.Helper()
 		if got != want{
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+	t.Run("saying hello with arguments", func(t *testing.T){
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying `Hello, world` in case of absence of arguments", func(t *testing.T){
 		got := Hello("")
 		want := "Hello, world"
-
-		if got != want{
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
