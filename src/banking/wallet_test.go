8fc2d3cfb07096d1d13b935afca829228f6ca9e3
Parth Gala <parthpgala@gmail.com>
2021-03-02 16:58:27 +0000

    Create function to withdraw bitcoin. Also, create a new type called `Bitcoin` to be used in place of int. Each place, the currency is stated with the BTC symbol. This is done using the Stringer interface. Write tests for withdraw function too. Next step should be to refactor the duplication in the Deposit and Withdraw function tests.

--- src/banking/wallet_test.go
+++ src/banking/wallet_test.go
@@ -4,12 +4,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("Deposit:", func (t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := 10
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw:", func (t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(0)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
